70bb25dea9a2799640d6b223aaab7e99c56e30e4
Adam Huffman <coronablue3@gmail.com>
2025-04-14 13:56:08 -0500

    Create basic route for POSTing conversions to server.py.

--- server.py
+++ server.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/convert', methods=['POST'])
+def convert():
+    value = request.form.get('value')
+    unit_from = request.form.get('from')
+    unit_to = request.form.get('to')
+
+    return { 'Value': value, 'From': unit_from, 'To': unit_to }
+
 if __name__ == '__main__':
     app.run(debug=True)
